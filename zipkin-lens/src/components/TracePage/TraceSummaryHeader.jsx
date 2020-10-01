566f23eaf1295eeb67e7a01895cf8cb0a9f76d8e
Maurizio Cescon <maurizio.cescon@gmail.com>
2020-10-01 16:03:23 +0200

    chore: eslint

--- zipkin-lens/src/components/TracePage/TraceSummaryHeader.jsx
+++ zipkin-lens/src/components/TracePage/TraceSummaryHeader.jsx
@@ -104,12 +104,12 @@ const TraceSummaryHeader = React.memo(({ traceSummary, rootSpanIndex }) => {
   const { i18n } = useLingui();
   const config = useUiConfig();
 
-  // eslint-disable
+  /*eslint-disable */
   const prettifyUrl =
     traceSummary
       ? ` http://santa-fe.cc.cec.eu.int:8411/prettify/${traceSummary.traceId}`
       : undefined;
-  // eslint-enabled
+  /*eslint-enable */
 
   const logsUrl =
     config.logsUrl && traceSummary
